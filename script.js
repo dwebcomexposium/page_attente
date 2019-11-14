93bd4cf07a4153777ab3be537a44ff26edbb04ea
ThomasRny <33687666+ThomasRny@users.noreply.github.com>
2019-11-14 17:13:26 +0100

    Add settings + responsive

---
+++ script.js
@@ -0,0 +1,20 @@
+$( document ).ready(function() {
+
+        $(".changeimagefix").click(function() {  //use a class, since your ID gets mangled
+          $(".jumbotron").toggleClass("imagefix").removeClass("imagegif");     //add the class to the clicked element
+        });
+        $(".changeimagegif").click(function() {  //use a class, since your ID gets mangled
+            $(".jumbotron").toggleClass("imagegif").removeClass("imagefix");      //add the class to the clicked element
+          });
+          $(".setting").click(function() {  //use a class, since your ID gets mangled
+            $(".containeroptions").toggleClass("settingopen");      //add the class to the clicked element
+          });
+
+          $('.pickedColor').keyup(function() {
+            $('body').get(0).style.setProperty("--main-color", $(this).val());
+        })
+        $('.imagePicker').keyup(function() {
+            $('body').get(0).style.setProperty("--main-image", "url('" + $(this).val() + "')");
+        }) 
+
+});
